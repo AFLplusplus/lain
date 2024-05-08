208e927bcf411f62f8a1f51ac2d9f9423a1ec5d3
Mrmaxmeier <Mrmaxmeier@gmail.com>
2024-05-08 16:20:59 +0200

    silence `non-local impl definition` warnings

--- lain_derive/src/dummy.rs
+++ lain_derive/src/dummy.rs
@@ -1,14 +1,10 @@
 use proc_macro2::{Ident, Span, TokenStream};
 
-use crate::internals::attr::unraw;
 use quote::quote;
 
 /// Wraps the code in a dummy const object. See https://github.com/serde-rs/serde/issues/159#issuecomment-214002626
-pub fn wrap_in_const(trait_: &str, ty: &Ident, code: TokenStream) -> TokenStream {
-    let dummy_const = Ident::new(
-        &format!("_IMPL_{}_FOR_{}", trait_, unraw(ty)),
-        Span::call_site(),
-    );
+pub fn wrap_in_const(_trait_: &str, _ty: &Ident, code: TokenStream) -> TokenStream {
+    let dummy_const = Ident::new("_", Span::call_site());
 
     let use_lain = quote! {
         #[allow(unknown_lints)]
